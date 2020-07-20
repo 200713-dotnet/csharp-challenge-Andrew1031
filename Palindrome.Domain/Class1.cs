21894c1b47a80a37e3931a30af2b58fba6ea5cb8
Andrew1031 <46513900+Andrew1031@users.noreply.github.com>
2020-07-20 13:40:05 -0700

    Update Class1.cs

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -1,8 +1,25 @@
-﻿using System;
+using System;
 
 namespace Palindrome.Domain
 {
     public class Class1
     {
+        public Boolean checkPalindrome(String s)
+        {
+          if (s.Length == 1 || s.Length == 0)
+          {
+            return true;
+          }
+            int firstIndex = 0;
+            int secondIndex  = s.Length - 1;
+            char firstPointer = s.charAt(firstIndex);
+            char secondPointer = s.charAt(secondIndex);
+
+            while (firstIndex < secondIndex)
+            {
+              
+            }
+
+        }
     }
 }

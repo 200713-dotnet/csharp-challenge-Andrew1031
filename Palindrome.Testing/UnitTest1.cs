0d655fb9f79001f7c51bb2de265d8d41cbb0b373
Andrew1031 <46513900+Andrew1031@users.noreply.github.com>
2020-07-20 13:59:26 -0700

    Update UnitTest1.cs

--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -1,5 +1,9 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Palindrome.Testing
 {
@@ -8,7 +12,14 @@ namespace Palindrome.Testing
         [Fact]
         public void Test1()
         {
-
+            String palindrome = "racecar";
+            String notPalindrome = "aaz";
+            String notPalindrome2 = "zaaf";
+            String palindrome2 = "lool";
+            Assert.AreEqual(true, isPalindrome(palindrome));
+            Assert.AreEqual(false, isPalindrome(notPalindrome));
+            Assert.AreEqual(false, isPalindrome(notPalindrome2));
+            Assert.AreEqual(true, isPalindrome(palindrome2));
         }
     }
 }
